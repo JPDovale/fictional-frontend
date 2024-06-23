cb98a5828c084680c208b6fab770342b215b11bf
joao <joao@192.168.1.16>
2024-06-23 03:08:54 -0300

    feat: add other image domain

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
       {
         hostname: 'pub-e7010998082d4cf29f84e4d3aa1f737f.r2.dev',
       },
+      {
+        hostname: 'pub-bc859334a56d4d2cb71576b27afa453e.r2.dev',
+      },
     ],
   },
 }
